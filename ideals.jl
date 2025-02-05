f6dabd82f4980e2b9a78c2bf397487b05736b3f0
Andrei Halanay <halanay@fmi.unibuc.ro>
2025-02-05 11:20:14 +0200

    restart

--- ideals.jl
+++ ideals.jl
@@ -2,6 +2,11 @@ using Oscar
 using Symbolics
 using Catalyst
 
+"""
+complex_balancing_ideal(rn::ReactionSystem)
+Computes the complex balancing ideal as defined in G.Craciun et al. *J. of Symbolic Computaion* **44**(2009) no.11, 1551-1565
+
+"""
 function complex_balancing_ideal(rn::ReactionSystem)
    in1=Catalyst.assemble_oderhs(rn,species(rn))
    f1,f2=Symbolics.build_function(in1,vcat(species(rn),parameters(rn))...)
